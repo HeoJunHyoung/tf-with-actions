9bd012db99a81276d4a53db42f5d077186c2025b
junbro <gjwnsgud4016@gmail.com>
2025-06-25 11:43:30 +0900

    add modules

---
+++ main.tf
@@ -0,0 +1,21 @@
+resource "aws_vpc" "backend-vpc" {
+  cidr_block       = "10.0.0.0/16"
+	
+  tags = {
+    Name = "tf-backend-lock"
+  }
+}
+
+module "s3" {
+  source = "./modules/s3"
+  bucket_name = "backend-bucket-h6bro-2"
+}
+
+module "backend" {
+  source = "./modules/backend"
+}
+
+module "dynamodb" {
+  source = "./modules/dynamodb"
+  table_name = "terraform-lock-2"
+}
